8e5d17aad14fcabbbb9762ca311f67c8d8a88c5a
BlendMax <Blendkozak@gmail.com>
2022-12-24 14:32:26 +0200

    add color

--- app/src/main/java/com/kozak/lightalarm/ui/theme/Color.kt
+++ app/src/main/java/com/kozak/lightalarm/ui/theme/Color.kt
@@ -6,3 +6,10 @@ val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+
+//////////////////////////////////////
+
+val FirstColor = Color(0xFF000000)
+val SecondColor = Color(0xFF1D1D1D)
+val ThirdColor = Color(0xFF0365FC)
+val FirstTextColor = Color(0xFFFFFFFF)
